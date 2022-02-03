f7e67354fdb1ee3b982a7bffe6da05bf9417645b
jecortesdev <jeison.f.cortes@gmail.com>
2022-02-03 02:03:00 -0500

    Se modica readme con la documentacion del proytecto y se crea dos arhivos para realizar las operaciones de las figuras

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # curso-practico-javascript
+
+...
+
+## Taller #1: Figuras Geometricas
+
+- Primer paso: definir las formulas
+- Segundo paso: implementar las formulas en JS
+- Tercer paso: crear funciones
+- Cuaro paso: Integrar JS con html
---
+++ figuras.js
@@ -0,0 +1,49 @@
+// Codigo del cuadrado
+
+console.group("Cuadrado")
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("EL permitero del cuadrado miden: " + perimetroCuadrado + " cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado ;
+console.log("El area del cuadrado miden: " + areaCuadrado + " cm2");
+console.groupEnd;
+
+// Codigo del triangulo
+console.group("Triangulo")
+
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los lados del triangulo miden: " 
+    + ladoTriangulo1 + " cm, " 
+    + ladoTriangulo2 + " cm, "
+    +  baseTriangulo + " cm"
+    );
+
+const alturaTriangulo = 5.5;
+console.log("La altura del triangulo es de: " + alturaTriangulo + " cm");
+
+const perimetroTrianguño = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
+
+console.log("El perimetro del triangulo es: " + perimetroTrianguño + " cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("EL area del triangulo es: " + areaTriangulo + " cm") ;
+
+console.groupEnd;
+
+// Codigo del Circulo
+console.group("Circulo")
+
+
+
+
+
+console.groupEnd;
