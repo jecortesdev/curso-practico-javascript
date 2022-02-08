d2c7609273666c35a8931e026b42685c968a677c
jecortesdev <jeison.f.cortes@gmail.com>
2022-02-07 23:52:51 -0500

    Se crea funcion para calculÃar la altura de un triangulo isoceles

--- figuras.js
+++ figuras.js
@@ -98,9 +98,35 @@ console.groupEnd;
 
 //Aqui interactuamos con el HTML
 function calcularPerimetroCuadrado(){
-    document.getElementById("input-cuadrado");
+    const input = document.getElementById("input-cuadrado");
+    const value = input.value;
+
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
     
 }
 function calcularAreaCuadrado(){
-    
+    const input = document.getElementById("input-cuadrado");
+    const value = input.value;
+
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+function alturaTriangulo(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    }else{
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+
 }
