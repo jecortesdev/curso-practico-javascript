26dbfa95f0da7761aecbe55d48922f4ec4ee910b
jecortesdev <jeison.f.cortes@gmail.com>
2022-02-14 22:59:31 -0500

    se crea archivo html y js  para hallar el promedio de una lista

---
+++ promedio.js
@@ -0,0 +1,27 @@
+const lista1 = [
+    100,
+    200,
+    300,
+    500
+];
+
+
+function calcularMediaArit(lista){
+    // let sumaLista = 0;
+
+    // for(let i = 0; i < lista.length; i++  ){
+    //     sumaLista = sumaLista + lista[i];
+    // }
+
+    const sumaLista = lista.reduce(
+        function(valorAcumulado = 0, nuevoElemento){
+            return valorAcumulado + nuevoElemento;
+
+        }
+    );
+
+    const promedioLista = sumaLista / lista.length;
+
+    return promedioLista;
+
+}
