103fc8450006d7ec1c59f13ea9f791e91c6dcf4f
jecortesdev <jeison.f.cortes@gmail.com>
2022-02-16 19:51:26 -0500

    Se crea archivo js para hallar la media de una lista

--- promedio.js
+++ promedio.js
@@ -8,20 +8,16 @@ const lista1 = [
 
 function calcularMediaArit(lista){
     // let sumaLista = 0;
-
     // for(let i = 0; i < lista.length; i++  ){
     //     sumaLista = sumaLista + lista[i];
     // }
-
     const sumaLista = lista.reduce(
         function(valorAcumulado = 0, nuevoElemento){
             return valorAcumulado + nuevoElemento;
 
         }
     );
-
     const promedioLista = sumaLista / lista.length;
-
     return promedioLista;
-
 }
+
